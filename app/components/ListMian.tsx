995a35c8132a1e4fc06fb4f55e7849effe546b6b
aliturab99 <syedyawaraliturab@gmail.com>
2024-06-09 16:24:14 +0400

    Nit and code improvement

--- app/components/ListMian.tsx
+++ app/components/ListMian.tsx
@@ -6,8 +6,8 @@ const listData = [
   { id: 1, name: "Scotland Island", area: "Sydney, Australia", imgSrc: "https://media.timeout.com/images/103835953/image.jpg" },
   { id: 2, name: "The Charles Grand Brasserie & Bar", area: "Lorem ipsum, Dolor", imgSrc: "https://thecharles.sydney/wp-content/uploads/sites/3/2022/11/KuzOkaGw-1.jpeg" },
   { id: 3, name: "Bridge Climb", area: "Dolor, Sit amet", imgSrc: "https://www.toledoblade.com/image/2021/06/21/1140x_a10-7_cTC/SCHAEDLER-JUMP.jpg" },
-  { id: 4, name: "Calm Bar", area: "Etcetera veni, Vidi vici", imgSrc: "https://media.timeout.com/images/103835953/image.jpg" },
-  { id: 5, name: "Vivid Festival", area: "Sydney, Australia", imgSrc: "https://media.timeout.com/images/103835953/image.jpg" },
+  { id: 4, name: "Calm Bar", area: "Etcetera veni, Vidi vici", imgSrc: "https://media.guestofaguest.com/t_article_content/wp-content/uploads/2014/06/clambar1.jpg" },
+  { id: 5, name: "Vivid Festival", area: "Sydney, Australia", imgSrc: "https://i.natgeofe.com/n/ae6a6b4d-1f7a-4b7f-822e-3f0179685923/ST_whatsnew_VividSydney2022_SydneyOperaHouse_Yarrkalpa_CREDITDestinationNSW_HR_16x9.jpg" },
 ];
 
 function ListMain() {
@@ -23,7 +23,7 @@ function ListMain() {
     const dragImage = document.createElement('div');
     dragImage.className = 'custom-drag-image';
     dragImage.innerHTML = `
-      <div style="display: flex; align-items: center; background: #fff">
+      <div style="display: flex; align-items: center">
         <img src="${items.find((item: any) => item.id === id).imgSrc}" alt="${items.find((item: any) => item.id === id).name}" style="width: 30px; height: 30px; margin-right: 10px;" />
         <h4>${items.find((item: any) => item.id === id).name}</h4>
       </div>
@@ -73,7 +73,7 @@ function ListMain() {
   };
 
   return (
-    <div className="flex flex-col items-center w-5/12 bg-white border border-gray-200 rounded-lg shadow relative">
+    <div className="flex flex-col items-center w-full md:w-5/12 bg-white border border-gray-200 rounded-lg shadow relative">
       <ul className="w-full">
         {items.map((item: any, index: number) => (
           <li
@@ -83,7 +83,7 @@ function ListMain() {
             onDragOver={(e) => handleDragOver(e, index)}
             onDrop={(e) => handleDrop(e, index)}
             onDragEnd={handleDragEnd}
-            className={`mb-4 ${getDropClassName(index)} ${draggedIndex === index ? 'opacity-30 bg-gray-200' : ''}`}
+            className={`${getDropClassName(index)} ${draggedIndex === index ? 'opacity-50 bg-gray-200' : ''}`}
           >
             <SingleListItem itemData={item} />
           </li>
